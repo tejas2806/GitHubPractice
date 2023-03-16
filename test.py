010f1abf060a61a8ab050c2ccb9621a9a96dbd3c
tejas2806 <tejaskhairnar28@gmil.com>
2023-03-16 12:18:23 +0530

    Subtraction Function Added

--- test.py
+++ test.py
@@ -2,3 +2,22 @@
 
 print("Hello World")
 print("AnotherPrint Added")
+
+#calculator project
+
+
+def addition(input1,input2):
+    res=input1+input2
+    print("Addition is " + res)
+
+
+def subtraction(input1, input2):
+    res=input1-input2
+    print("Subtraction is "+ res)
+
+
+    
+
+
+
+
